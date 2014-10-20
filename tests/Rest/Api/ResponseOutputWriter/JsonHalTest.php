34dd167b0191de03c7f394727b7ec0d9c8a28159
Andreas Schleifer <aschleifer@bigpoint.net>
2014-10-20 12:30:25 +0200

    fix unit tests after response refactoring

--- tests/Rest/Api/ResponseOutputWriter/JsonHalTest.php
+++ tests/Rest/Api/ResponseOutputWriter/JsonHalTest.php
@@ -1,14 +1,14 @@
 <?php
-namespace Rest\Api\Response;
+namespace Rest\Api\ResponseOutputWriter;
 
 use \Rest\Api\DataObject;
 
 /**
- * Class JsonTest
+ * Class JsonHalTest
  *
- * @package Rest\Api\Response
+ * @package Pit\Api\ResponseOutputWriter
  */
-class JsonTest extends \PHPUnit_Framework_TestCase
+class JsonHalTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
@@ -26,7 +26,7 @@ class JsonTest extends \PHPUnit_Framework_TestCase
     private $_mockHeaders = null;
 
     /**
-     * @var \Rest\Api\Response\Json
+     * @var \Rest\Api\ResponseOutputWriter\JsonHal
      */
     private $_candidate = null;
 
@@ -55,7 +55,7 @@ class JsonTest extends \PHPUnit_Framework_TestCase
             )
         );
 
-        $this->_candidate = new Json(
+        $this->_candidate = new JsonHal(
             $this->_mockRequest,
             $this->_mockResponse,
             $this->_mockHeaders,
@@ -64,35 +64,48 @@ class JsonTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider outputProvider
+     * @param string           $path
+     * @param array|DataObject $data
+     *
+     * @dataProvider writeProvider
      */
-    public function testOutput($data)
+    public function testWrite($path, $data)
     {
+        $this->_mockRequest
+            ->expects($this->exactly(1))
+            ->method('getPath')
+            ->will($this->returnValue($path));
         $this->_mockHeaders->expects($this->exactly(1))->method('set');
         $this->_mockResponse->expects($this->exactly(1))->method('setStatus');
         $this->_mockResponse->expects($this->exactly(1))->method('setBody');
 
-        $this->_candidate->output($data, 200);
+        $this->_candidate->write($data, 200);
     }
 
-    public function outputProvider()
+    public function writeProvider()
     {
         $dataObject = new DataObject(
             array(
                 'affiliateId' => 415,
                 'gameId'      => 14,
             ),
-            array()
+            array(),
+            array(
+                'mockLinkRel' => 'mockLinkUri',
+            )
         );
 
         return array(
             array(
+                'path' => '/global-footer/415/22',
                 'data' => $dataObject,
             ),
             array(
+                'path' => '/global-footer/415',
                 'data' => array(),
             ),
             array(
+                'path' => '/global-footer',
                 'data' => array(
                     $dataObject,
                     $dataObject,
