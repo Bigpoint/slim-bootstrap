713218daa74d7a0ea004716071cf0ad697353e73
Andreas Schleifer <aschleifer@bigpoint.net>
2014-10-30 12:53:01 +0100

    TP-5989 / add comment to interface

--- src/Rest/Api/Endpoint/InjectClientId.php
+++ src/Rest/Api/Endpoint/InjectClientId.php
@@ -4,6 +4,10 @@ namespace Rest\Api\Endpoint;
 /**
  * Interface InjectClientId
  *
+ * This interface provides a function to set the clientId into the endpoint.
+ * If an endpoint implements this interface the clientId will be injected before
+ * the endpoint is called.
+ *
  * @package Rest\Api\Endpoint
  */
 interface InjectClientId
